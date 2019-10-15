0c10d2dc4c6e8685470b193364d9ab1e03efd3a2
zry666333 <13262512337@163.com>
2019-10-15 14:05:12 +0800

    加入serviceId唯一性判定

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,1 +1,14 @@
-//
+// 判断一个数组对象中，是否有相同的对象属性值
+export function isRepeat (obj, array, key) {
+  let flag = false
+  if (array instanceof Array && array.length !== 0) {
+    array.forEach((item, index) => {
+      if (item[key] === obj[key]) {
+        flag = true
+      }
+    })
+    return flag
+  } else {
+    return flag
+  }
+}
