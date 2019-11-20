4303db1290c358965bcd10362d0820ac7f1b8fd5
zry666333 <13262512337@163.com>
2019-11-20 10:26:44 +0800

    将axios进行了新的封装

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import '../css/reset.scss'
-import '../css/global.scss'
 import Home from '@/components/home'
 import Login from '@/components/login'
 
